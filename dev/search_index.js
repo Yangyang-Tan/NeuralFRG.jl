5039b18fe9cf2a74a66ebeaffb36a132bdc34fff
Documenter.jl <documenter@juliadocs.github.io>
2025-04-04 07:22:32 +0000

    build based on d276280

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeuralFRG","category":"page"},{"location":"#NeuralFRG","page":"Home","title":"NeuralFRG","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeuralFRG.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeuralFRG]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeuralFRG","category":"page"},{"location":"#NeuralFRG","page":"Home","title":"NeuralFRG","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeuralFRG.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeuralFRG]\nPrivate = false","category":"page"}]
 }
